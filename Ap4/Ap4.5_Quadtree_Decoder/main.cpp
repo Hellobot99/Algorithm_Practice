36fd1853a86b4177a47cddf4e5fb003330d26656
ktj4060 <ktj4060@naver.com>
2025-04-22 00:11:14 +0900

    modified 4.5

--- Ap4/Ap4.5_Quadtree_Decoder/main.cpp
+++ Ap4/Ap4.5_Quadtree_Decoder/main.cpp
@@ -9,32 +9,36 @@
 #include <string>
 using namespace std;
 
-void solve(int N, vector<vector<int>> arr, int x, int y) {
-    if (N == 1) {
-        if (arr[x][y]) cout << "b";
-        else cout << "w";
-    }
-    else {
-        int sum = 0;
-        for (int i = x; i < N + x; i++) {
-            for (int k = y; k < N + y; k++) {
-                sum += arr[i][k];
+string a;
+int s_idx = 0;
+
+void solve(int N, vector<vector<int>>& arr, int x, int y) {
+    if (a.size() - 1 < s_idx) return;
+    int m = N / 2;
+    switch (a[s_idx]) {
+    case 'x':
+        s_idx++;
+        solve(m, arr, x, y);
+        solve(m, arr, x, y + m);
+        solve(m, arr, x + m, y);
+        solve(m, arr, x + m, y + m);
+        break;
+    case 'w':
+        s_idx++;
+        for (int i = x; i < x + N; i++) {
+            for (int k = y; k < y + N; k++) {
+                arr[i][k] = 0;
             }
         }
-        if (sum == 0) {
-            cout << "w";
+        break;
+    case 'b':
+        s_idx++;
+        for (int i = x; i < x + N; i++) {
+            for (int k = y; k < y + N; k++) {
+                arr[i][k] = 1;
+            }
         }
-        else if (sum == (N * N)) {
-            cout << "b";
-        }
-        else {
-            int m = N / 2;
-            cout << "x";
-            solve(m, arr, x, y);
-            solve(m, arr, x, y + m);
-            solve(m, arr, x + m, y);
-            solve(m, arr, x + m, y + m);
-        }
+        break;
     }
 }
 
@@ -42,14 +46,19 @@ int main() {
 
     int N;
     cin >> N;
+
+    cin >> a;
     vector<vector<int>> arr(N, vector<int>(N, 0));
-    for (int i = 0; i < N; i++) {
-        for (int k = 0; k < N; k++) {
-            cin >> arr[i][k];
-        }
-    }
 
     solve(N, arr, 0, 0);
+
+    cout << N << endl;
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            if (k == N - 1) cout << arr[i][k] << endl;
+            else cout << arr[i][k] << " ";
+        }
+    }
 
     return 0;
 }
